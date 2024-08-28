ad8db25bc9cb26336015f9a596132c8879d35a86
ChiuzuChilumbuRD <chiuzu.chilumbu@metaguru.com.tw>
2024-08-28 17:28:22 +0800

    first hard reset after reena branch merge

--- MyFirstMauiApp/obj/Debug/net8.0-ios/iossimulator-arm64/MyFirstMauiApp.AssemblyInfo.cs
+++ MyFirstMauiApp/obj/Debug/net8.0-ios/iossimulator-arm64/MyFirstMauiApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstMauiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3720a39a3eca091c511ce6837817635dc911821e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+95a7d89559cae7f91355bce507ac89d36f3f1cc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstMauiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstMauiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

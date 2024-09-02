2e2c594bf2e9a823c4c171614355d0b9649c2c44
ChiuzuChilumbuRD <chiuzu.chilumbu@metaguru.com.tw>
2024-09-02 10:41:07 +0800

    Test Run

--- MyFirstMauiApp/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MyFirstMauiApp.AssemblyInfo.cs
+++ MyFirstMauiApp/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MyFirstMauiApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstMauiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3720a39a3eca091c511ce6837817635dc911821e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e8a4c77ceffb2f0dd6604d0a44bf8a76652705c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstMauiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstMauiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
